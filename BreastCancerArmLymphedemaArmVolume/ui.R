004d17e099da54cc8fefb11a75ee5d41a7141975
Alex Zajichek <alexzajichek@gmail.com>
2023-07-20 16:07:32 -0400

    Added source code links AB#40010

--- BreastCancerArmLymphedemaArmVolume/ui.R
+++ BreastCancerArmLymphedemaArmVolume/ui.R
@@ -63,8 +63,14 @@ fluidPage(theme = shinytheme("flatly"),
                       href = "javascript:myFunction()",
  
                       style = "font-family: 'Lato','Helvetica Neue',Helvetica,Arial,sans-serif;
-                      font-size: 15px;color: #2c3e50;font-weight: bold;text-align: center;text-decoration: underline;"), 
+                      font-size: 15px;color: #2c3e50;font-weight: bold;text-align: center;text-decoration: underline;"),
+                    " | ",
+                    a("Source Code", 
+                      href = "https://github.com/ClevelandClinicQHS/riskcalc-website/tree/main/BreastCancerArmLymphedemaArmVolume", 
+                      style = "font-family: 'Lato','Helvetica Neue',Helvetica,Arial,sans-serif;font-size: 15px;color: #2c3e50;font-weight: bold;text-align: center;text-decoration: underline;"
+                    ),
                     style = "text-align: center;"),
+                  
                   img(src='../logo-ccf.png', style = "max-width:30%; max-height:100%;")
               )
           )
