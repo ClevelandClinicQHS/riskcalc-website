6a2d9964bc717308116e122ae443b9b5d9a97015
martynb-ccf <132491365+martynb-ccf@users.noreply.github.com>
2023-10-23 13:45:41 -0400

    added author information and disclaimer

--- KidneyUpstaging/ui.R
+++ KidneyUpstaging/ui.R
@@ -1,3 +1,4 @@
+#version October 2023, no server changes.
 library(shiny)
 
 shinyUI(
@@ -9,7 +10,6 @@ shinyUI(
         
         navbarPage(
             title = "Kidney cancer upstaging risk calculator", inverse = T,
-            tabPanel("Home",
                      sidebarLayout(
                          sidebarPanel(
                              numericInput("Age", "Age [years]", NA, min = 18.0, max = 100.0),
@@ -38,13 +38,45 @@ shinyUI(
                                           HTML('<h3>Preamble</h3>
               <p>This risk calculator is intended for patients who have had a detection of a Stage 1 clinically localized kidney tumor with no evidence of metastasis (i.e. spread). The tumor is characterized based on either computed tomography (CT) or magnetic resonance imaging (MRI) scans; typically these studies are contrast-enhanced. In addition, the calculator applies to those kidney tumors which are believed to represent a primary tumor arising from the parenchyma (or body) of the kidney. In other words, if this kidney tumor is believed to originate from the urothelium, or ‘renal pelvis’, then this calculator is not applicable. Similarly, if the kidney tumor is felt to represent metastasis (i.e. spread from another cancer elsewhere in the body) then this should not be utilized. The determination of the ‘Intended surgery’ is based on the overall impression of the surgeon who will perform the procedure after a thorough assessment of the history, physical examination, laboratory values, and cross-sectional imaging (i.e. CT or MRI).</p>
               <p>For detailed description of the variables included in the calculator, click on the "More information" tab.</p>
-')),
+                                                  '),
+                                          
+                HTML('<h3>Author Information</h3>
+                     <p> Pruthi DK, Miller G, Ankerst DP, Neumair M, Capitanio U, Correa AF, 
+                     Lane BR, Roussel E, McGregor TB, Derweesh IH, Cordeiro M, Pierorazio PM, 
+                     Calvo C, Bi H, Noyes SL, Meagher M, Kutikov A, Uzzo RG, Van Poppel H, Larcher A, 
+                     Montorsi F, Kattan MW, Kaushik D, Liss MA. 
+                     
+                     <a href = "https://www.auajournals.org/doi/epdf/10.1097/JU.0000000000003650">
+                     Diabetes, Obesity, and Pathological Upstaging in Renal Cell Carcinoma: 
+                     Results From a Large Multi-institutional Consortium.</a> J Urol. 2023;210(5):750-762.</p>
+                     
+                      
+                
+                                   
+                     
+                     
+                     </p>
+                     
+                     '),                         
+                HTML('<h3>Disclaimer</h3> 
+                     <p>No Medical Advice. ALTHOUGH SOME CONTENT MAY BE PROVIDED BY INDIVIDUALS IN THE MEDICAL PROFESSION, 
+YOU ACKNOWLEDGE THAT PROVISION OF SUCH CONTENT DOES NOT CREATE A MEDICAL PROFESSIONAL-PATIENT RELATIONSHIP 
+AND DOES NOT CONSTITUTE AN OPINION, MEDICAL ADVICE, PROFESSIONAL DIAGNOSIS, SERVICE OR TREATMENT OF ANY CONDITION. 
+Access to general information is provided for educational purposes only, through this site and links to other sites.
+Content is not recommended or endorsed by any doctor or healthcare provider. 
+The information and Content provided are not substitutes for medical or professional care, 
+and you should not use the information in place of a visit, call, consultation or the advice of your physician 
+or other healthcare provider. You are liable or responsible for any advice, course of treatment, diagnosis or any other information, 
+services or product obtained through this site.</p>'),                          
+                                          
+                                          ),
                                  tabPanel("More Information",
                                           info_en)
+
+
                              )
                          )
                      )   
-            )
             #tabPanel("Map", stats_en)
         )
     )
