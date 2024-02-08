35e123307aa50886b217ebcc2e5d58b27dc5672e
Alex Zajichek <alexzajichek@gmail.com>
2024-02-08 11:12:44 -0500

    Added stratified models AB#112974

--- AntibioticOptimization/global.R
+++ AntibioticOptimization/global.R
@@ -1,8 +1,60 @@
 # Load packages
 library(shiny)
 library(shinythemes)
-library(gbm3)
+library(gbm)
 
-# Import the model
+## Import the metadata
 load(file = "modeling_dataset_metadata.RData")
-load(file = "prod_model.RData")
+
+## Import the models
+
+# Get the model list
+model_list <- list.files("models")
+
+# Iterate the list a load
+for(i in model_list) 
+  load(file = paste0("models/", i))
+
+# Function to get predictions for an input set of predictors (for each antibiotic)
+get_predictions <-
+  function(pred_dat) {
+    
+    # Get the predictions from each model
+    temp_preds <- 
+      data.frame(
+        Antibiotic = 
+          c(
+            "Ampicillin",
+            "Cefazolin",
+            "Cefepime",
+            "Ceftriaxone",
+            "Ciprofloxacin",
+            "Gentamicins",
+            "Meropenem",
+            "Nitrofurantoin",
+            "Piperacillin-Tazobactam Combination",
+            "Trimethoprim-Sulfamethoxazole Combination",
+            "Vancomycin"
+          ),
+        Susceptibility = 
+          c(
+            predict(ampicillin, newdata = pred_dat, n.trees = ampicillin$n.trees, type = "response"),
+            predict(cefazolin, newdata = pred_dat, n.trees = cefazolin$n.trees, type = "response"),
+            predict(cefepime, newdata = pred_dat, n.trees = cefepime$n.trees, type = "response"),
+            predict(ceftriaxone, newdata = pred_dat, n.trees = ceftriaxone$n.trees, type = "response"),
+            predict(ciprofloxacin, newdata = pred_dat, n.trees = ciprofloxacin$n.trees, type = "response"),
+            predict(gentamicins, newdata = pred_dat, n.trees = gentamicins$n.trees, type = "response"),
+            predict(meropenem, newdata = pred_dat, n.trees = meropenem$n.trees, type = "response"),
+            predict(nitrofurantoin, newdata = pred_dat, n.trees = nitrofurantoin$n.trees, type = "response"),
+            predict(piperacillin, newdata = pred_dat, n.trees = piperacillin$n.trees, type = "response"),
+            predict(trimethoprim, newdata = pred_dat, n.trees = trimethoprim$n.trees, type = "response"),
+            predict(vancomycin, newdata = pred_dat, n.trees = vancomycin$n.trees, type = "response")
+          )
+      )
+    
+    # Compute the resistance probability
+    temp_preds$Resistance <- 1 - temp_preds$Susceptibility
+    
+    temp_preds
+    
+  }
--- AntibioticOptimization/server.R
+++ AntibioticOptimization/server.R
@@ -24,59 +24,29 @@ shinyServer(function(input, output, session){
         # Validate the inputs
         validate(need(!is.na(input$age) & input$age >= 18, "Please enter an age 18 years or older."))
         
-        # Make antibiotic list
-        antibiotics <- 
-          c(
-            "Trimethoprim-Sulfamethoxazole Combination",
-            "Nitrofurantoin",
-            "Ciprofloxacin",
-            "Cefazolin",
-            "Ceftriaxone",
-            "Ampicillin",
-            "piperacillin-tazobactam combination",
-            "Vancomycin",
-            "cefepime",
-            "meropenem",
-            "Gentamicins"
-          )
-        
         # Make a data frame
-        temp_dat <- 
-          data.frame(
-            AntibioticCategory = as.factor(antibiotics),
-            AnyOrganism_DaysSincePreviousResistance = as.factor(input$days_since_resistance),
-            DepartmentSpecialty = as.factor(input$specialty),
-            Facility = as.factor(input$facility),
-            ORGANISM_HX_112283007 = as.factor(input$hx_ecoli),
-            CollectionMethod = as.factor(input$collection),
-            LivingArrangement = as.factor(input$living_arrangement),
-            MobilityStatus = as.factor(input$mobility),
-            Age = input$age,
-            Sex = as.factor(input$sex),
-            Ampicillin_HX = as.factor(input$hx_ampicillin),
-            Cefazolin_HX = as.factor(input$hx_cefazolin),
-            cefepime_HX = as.factor(input$hx_cefepime),
-            Ceftriaxone_HX = as.factor(input$hx_ceftriaxone),
-            Ciprofloxacin_HX = as.factor(input$hx_ciprofloxacin),
-            Gentamicins_HX = as.factor(input$hx_gentamicins),
-            meropenem_HX = as.factor(input$hx_meropenem),
-            Nitrofurantoin_HX = as.factor(input$hx_nitrofurantoin),
-            piperacillin_tazobactam_combination_HX = as.factor(input$hx_piperacillin),
-            Trimethoprim_Sulfamethoxazole_Combination_HX = as.factor(input$hx_trimethoprim),
-            Vancomycin_HX = as.factor(input$hx_vancomycin)
-          )
-        
-        # Get the predictions from the model
-        temp_dat$Susceptibility <- predict(prod_model, newdata = temp_dat, n.trees = prod_model$params$num_trees, type = "response")
-        
-        # Add the resistance probability
-        temp_dat$Resistance <- 1 - temp_dat$Susceptibility
-        
-        # Clean the format
-        temp_dat$Susceptibility <- round(temp_dat$Susceptibility * 100, 1)
-        temp_dat$Resistance <- round(temp_dat$Resistance * 100, 1)
-        
-        temp_dat
+        data.frame(
+          AnyOrganism_DaysSincePreviousResistance = as.factor(input$days_since_resistance),
+          DepartmentSpecialty = as.factor(input$specialty),
+          Facility = as.factor(input$facility),
+          ORGANISM_HX_112283007 = as.factor(input$hx_ecoli),
+          CollectionMethod = as.factor(input$collection),
+          LivingArrangement = as.factor(input$living_arrangement),
+          MobilityStatus = as.factor(input$mobility),
+          Age = input$age,
+          Sex = as.factor(input$sex),
+          Ampicillin_HX = as.factor(input$hx_ampicillin),
+          Cefazolin_HX = as.factor(input$hx_cefazolin),
+          cefepime_HX = as.factor(input$hx_cefepime),
+          Ceftriaxone_HX = as.factor(input$hx_ceftriaxone),
+          Ciprofloxacin_HX = as.factor(input$hx_ciprofloxacin),
+          Gentamicins_HX = as.factor(input$hx_gentamicins),
+          meropenem_HX = as.factor(input$hx_meropenem),
+          Nitrofurantoin_HX = as.factor(input$hx_nitrofurantoin),
+          piperacillin_tazobactam_combination_HX = as.factor(input$hx_piperacillin),
+          Trimethoprim_Sulfamethoxazole_Combination_HX = as.factor(input$hx_trimethoprim),
+          Vancomycin_HX = as.factor(input$hx_vancomycin)
+        )
         
       }
     )
@@ -84,27 +54,33 @@ shinyServer(function(input, output, session){
   # Make result table
   output$result <-
     DT::renderDataTable({
-      pred_dat <- data()[,c("AntibioticCategory", "Susceptibility", "Resistance")]
       
-      # Change the name
-      names(pred_dat)[names(pred_dat) == "AntibioticCategory"] <- "Antibiotic"
+      # Get the predicted values for each antibiotic
+      temp_pred <- get_predictions(data())
       
-      pred_dat
-      
-    },
-    options = 
-      list(
-        pageLength = 100,
-        lengthMenu = 0,
-        searching = 0,
-        processing = 0,
-        info = 0,
-        paging = 0,
-        initComplete = I(
-          "function(settings, json) {
+      # Make the data table
+      temp_dt <- 
+        DT::datatable(
+          temp_pred,
+          options = 
+            list(
+              pageLength = 100,
+              lengthMenu = 0,
+              searching = 0,
+              processing = 0,
+              info = 0,
+              paging = 0,
+              initComplete = DT::JS(
+                "function(settings, json) {
                     $(this.api().table().header()).css({'background-color': '#606060', 'color': '#fff'});
             }")
-      )
-    )
+            ),
+          rownames = FALSE
+        )
+      
+      # Convert to percentage
+      DT::formatPercentage(temp_dt, columns = c("Susceptibility", "Resistance"))
+      
+    })
   
 })
